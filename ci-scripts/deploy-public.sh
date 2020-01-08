4379cfed32554149b8be9935b41321f83bc55791
Brian Gu <briangu33@gmail.com>
2020-01-08 15:59:42 -0500

    remove aztec-specific directories and build processes

--- ci-scripts/deploy-public.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker push aztecprotocol/$1:latest
-if [ -n "$CIRCLE_SHA1" ]; then
-  docker push aztecprotocol/$1:$CIRCLE_SHA1
-fi
--- job-server/redis-scripts/setup.lua
+++
@@ -1,7 +0,0 @@
-local from = tonumber(ARGV[1])
-local numberOfJobs = tonumber(ARGV[2])
-
-redis.pcall('flushall')
-redis.pcall('set', 'from', from)
-redis.pcall('set', 'to', from + numberOfJobs)
-redis.pcall('del', 'pendingJobs')

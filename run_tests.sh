15be9a3a29144fa30e4b78d071d42ba09c00ac07
Divide-By-0 <aayushg@mit.edu>
2020-01-08 00:49:10 -0500

    added hot reload testing sh file

---
+++ run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# resets a running server cmd, 
+SIG="X-Signature:0x57d537ce2418c6e4ffcdf1452d90c35a44bbfdd1ba4356ea09ec1cd4a1bfb97c5e1967f784b51694585f726c205121f68ddddef7aec6316ccc3356dc5cd9ced61b"
+CT="Content-Type:application/json;charset=utf-8"
+cd setup-mpc-client
+docker build -t aztecprotocol/setup-mpc-client .
+cd ../
+curl -X POST -i -H $CT -H $SIG -c cookies.txt -X POST http://localhost:8081/api/reset -d "$(cat <<EOF
+{
+	"startTime": 10,
+	"endTime": 60,
+	"selectBlock": -1,
+	"invalidateAfter": 7600,
+	"numG1Points": 1008000,
+	"numG2Points": 1,
+	"pointsPerTranscript": 50400,
+	"maxTier2": 5,
+	"minParticipants": 2,
+	"participants0": [
+		"0x405b42dDb31d3250087B230407250b2Cf9050971",
+		"0xA5f146eCb90A624de0433530634566313758f924",
+		"0x1aA18F5b595d87CC2C66d7b93367d8beabE203bB"
+	]
+}
+EOF
+)"

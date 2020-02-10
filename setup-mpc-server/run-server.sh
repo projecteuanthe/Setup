ee824ffc1bc6d72877baa378669bd6deb3aaab77
Brian Gu <briangu33@gmail.com>
2020-02-10 01:12:00 -0500

    add volume flag to run-server

--- setup-mpc-server/run-server.sh
+++ setup-mpc-server/run-server.sh
@@ -3,11 +3,15 @@ if [[ -z "$PORT" ]]; then
   PORT="8081" # default port 8081
 fi
 
+if [[ -z "$VOLUME" ]]; then
+  VOLUME="mpc-server-vol"
+fi
+
 if [[ $* == *--clear-state* ]]; then
   # remove all stopped setup-mpc-server containers
   docker ps -a | awk '{ print $1,$2 }' | grep setup-mpc-server:latest | awk '{print $1 }' | xargs -I {} docker rm {}
   # remove mpc-server-vol volume
-  docker volume rm mpc-server-vol
+  docker volume rm "$VOLUME"
 fi
 
-docker run --mount 'type=volume,src=mpc-server-vol,dst=/usr/src/setup-mpc-server/store' -p "$PORT":80 setup-mpc-server:latest
+docker run --mount 'type=volume,src='"$VOLUME"',dst=/usr/src/setup-mpc-server/store' -p "$PORT":80 setup-mpc-server:latest

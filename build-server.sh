7750327692140d3d1c1b31efab5dce7b17c23b95
Brian Gu <briangu33@gmail.com>
2020-03-18 22:44:27 -0400

    add yarn install to build-server

--- build-server.sh
+++ build-server.sh
@@ -6,9 +6,11 @@ cargo build --release --bin new
 mv target/release/verify_contribution ../../
 mv target/release/new ../../
 cd ../../../setup-mpc-common
+yarn install
 yarn build
 yarn link
 cd ../setup-mpc-server
 ../setup-tools/new ./initial/circuit.json ./initial/initial_params ./initial/radix
+yarn install
 yarn link setup-mpc-common
 yarn build
